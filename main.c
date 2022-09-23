decc008eafe954f36d0ed4a91fdc753bfc4d5021
Eldar-Saltykov <112901922+Eldar-Saltykov@users.noreply.github.com>
2022-09-23 14:07:08 +0300

    инд. задание

--- main.c
+++ main.c
@@ -1,45 +1,38 @@
-#include <stdio.h>
-#include <locale.h>
-#define D  2.54
-#define P  2.32166
+#include<stdio.h>
+#include<locale.h>
 
-void main()
+int s_plo(int n)
 {
-	setlocale(LC_ALL, "RUS");
-	int num1, num2;
-	puts("Введите число");
-	scanf("%d", &num1);
-	printf("Выведенно число %d\n\t ", num1);
+	int res1 = n / 100;
+	return res1;
+}
+int s_plo2(int n)
+{
+	int res2 = (n / 10) % 10;
+	return res2;
+}
+int s_plo3(int n)
+{
+	int res3 = n % 10;
+	return res3;
+}
 
-	puts("Введите 2 число\n\t");
-	scanf("%d", &num2);
-	printf("Сумма = %d , Разность = %d, Произведение = %d, Частное = %d, Остаток = %d\n",
-		num2 + num1, num2 - num1, num2 * num1, num2 / num1, num2 % num1);
-	int   dym;
 
-	float result1, result2;
-	puts("Введите значение для рассчёта");
-	scanf("%d", &dym);
-	result1 = D * dym;
-	result2 = P * dym;
-	printf(" % d дюймов – это % .2f см\n", dym, result1);
-	printf(" % d пулгада – это % .7f см\n", dym, result2);
+  int main()
+{
+	  int n;
+	  int res1, res2, res3;
+   setlocale(LC_ALL, "RUS");
+	puts("Введите трёхзначное число");
 
-	int a, b, res1, res2, res3;
-	puts("Введите число а");
-	scanf("%d", &a);
-	puts("Введите число b");
-	scanf("%d", &b);
-	res1 = a * b;
-	res2 = a + b;
-	res3 = a - b;
-	printf("-------------------------------\n");
-	printf("|  a *  b |  a +  b |  a -  b |\n");
-	printf("-------------------------------\n");
-	printf("| %2d * %2d | %2d + %2d | %2d - %2d |\n", a, b, a, b, a, b);
-	printf("-------------------------------\n");
-	printf("|    %2d   |   %2d    |    %2d   |\n", res1, res2, res3);
-
+	scanf("%d", &n);
+	res1 = s_plo (n);
+	res2 = s_plo2( n);
+	res3 = s_plo3( n);
+    puts("Введите трёхзначное число");
+	
+	printf("В этом числе\n сотен : %.d\n десятков : %.d\n единиц : %.d", res1, res2, res3);
+	
 }
 
 
